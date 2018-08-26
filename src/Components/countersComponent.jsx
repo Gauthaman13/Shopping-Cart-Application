1f7342701177727f1b8ebd099b636a797ee09481
Gauthaman1392 <gauthammukesh92@gmail.com>
2018-08-25 23:36:25 -0400

    Spread operators added for counter increments

--- src/Components/countersComponent.jsx
+++ src/Components/countersComponent.jsx
@@ -3,26 +3,48 @@ import Counter from "./counterComponent";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, vlaue: 2 },
-      { id: 2, vlaue: 5 },
-      { id: 3, vlaue: 7 },
-      { id: 4, vlaue: 0 }
+      { id: 1, value: 2 },
+      { id: 2, value: 5 },
+      { id: 3, value: 7 },
+      { id: 4, value: 0 }
     ]
   };
   render() {
     return (
       <div>
+        <button
+          onClick={this.resetCounters}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
-            value={counter.vlaue}
+            onIncrement={this.handleIncrements}
             onDelete={this.handleDelete}
-            id={counter.id}
+            counter={counter}
           />
         ))}
       </div>
     );
   }
+
+  handleIncrements = incrementedCounter => {
+    const counters = [...this.state.counters];
+    const counterIndex = counters.indexOf(incrementedCounter);
+    counters[counterIndex] = { ...incrementedCounter };
+    counters[counterIndex].value++;
+    this.setState({ counters });
+  };
+
+  resetCounters = () => {
+    const updatedCounter = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ updatedCounter });
+  };
 
   handleDelete = deletedCounter => {
     const counters = this.state.counters.filter(
